6443eb20f7bb3a6d28f90716ea49fcaf88ef8cf4
agakoz <agataakoziol@gmail.com>
2020-03-14 20:42:26 +0100

    style changes in to-do list: ngStyle and ngClass exploration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'toDoListYTProject';
 
   tasksList: Array<string> = [];
@@ -22,5 +22,9 @@ export class AppComponent {
   done(task: string) {
     this.doneList.push(task);
     this.remove(task);
+  }
+
+  ngOnInit(): void {
+    this.tasksList = ['Zmywanie', 'Nauka', 'Zakupy', 'Podlewanie kwiatków' ];
   }
 }
