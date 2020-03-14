fed878ae3547eb5a277bd741c7964e7f4d11189e
agakoz <agataakoziol@gmail.com>
2020-03-14 19:43:44 +0100

    Code refactored: app component divided into child components

---
+++ src/app/todo-task/todo-task.component.ts
@@ -0,0 +1,32 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-todo-task',
+  templateUrl: './todo-task.component.html',
+  styleUrls: ['./todo-task.component.css']
+})
+export class TodoTaskComponent implements OnInit {
+
+  constructor() {
+  }
+
+  @Input()
+  tasksList = [];
+
+  @Output()
+  emitDone = new EventEmitter<string>();
+
+  @Output()
+  emitRemove = new EventEmitter<string>();
+
+  ngOnInit(): void {
+  }
+
+  remove(task: string) {
+    this.emitRemove.emit(task);
+  }
+
+  done(task: string) {
+    this.emitDone.emit(task);
+  }
+}
